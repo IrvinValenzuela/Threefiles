c2a629379492442e7b36b144da1e242a7caa492c
Irvin Valenzuela <irvinvalenzuela411@gmail.com>
2026-04-18 23:42:59 -0600

    Add files via upload

---
+++ nuevas funciones.py
@@ -0,0 +1,8 @@
+#creear un diccionario con tu informacion personal
+mi_dict = {"nombre": "irvin", 
+           "edad": "19", 
+           "ciudad": "escuintla, escuintla",
+           "lenguajeFAV": "android estudio",
+           "universidad": "san pablo" }
+
+print(mi_dict.items()) #muestra el diccionario en forma de tuplas
