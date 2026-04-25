6d827da3f82aec764c397574f3ba11b30bedad88
Irvin Valenzuela <irvinvalenzuela411@gmail.com>
2026-04-24 20:23:20 -0600

    Delete nuevas funciones.py

--- nuevas funciones.py
+++
@@ -1,8 +0,0 @@
-#creear un diccionario con tu informacion personal
-mi_dict = {"nombre": "irvin", 
-           "edad": "19", 
-           "ciudad": "escuintla, escuintla",
-           "lenguajeFAV": "android estudio",
-           "universidad": "san pablo" }
-
-print(mi_dict.items()) #muestra el diccionario en forma de tuplas
